addcaa4d969bf54e37b4bc0fdf907f3ec84c975e
squal199 <hello@squal199.com>
2024-04-18 02:22:34 +0100

    Add the login and logout links

---
+++ app/auth/Provider.tsx
@@ -0,0 +1,10 @@
+/** @format */
+"use client";
+import { PropsWithChildren } from "react";
+import { SessionProvider } from "next-auth/react";
+
+const AuthProvider = ({ children }: PropsWithChildren) => {
+  return <SessionProvider>{children}</SessionProvider>;
+};
+
+export default AuthProvider;
--- app/layout.tsx
+++ app/layout.tsx
@@ -8,6 +8,7 @@ import { Inter } from "next/font/google";
 import { Container, Theme } from "@radix-ui/themes";
 import NavBar from "./NavBar";
 import config from "./../tailwind.config";
+import AuthProvider from "./auth/Provider";
 
 const inter = Inter({
   subsets: ["latin"],
@@ -27,12 +28,14 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className={inter.variable}>
-        <Theme appearance="light" accentColor="violet">
-          <NavBar />
-          <main className="p-5">
-            <Container>{children}</Container>
-          </main>
-        </Theme>
+        <AuthProvider>
+          <Theme appearance="light" accentColor="violet">
+            <NavBar />
+            <main className="p-5">
+              <Container>{children}</Container>
+            </main>
+          </Theme>
+        </AuthProvider>
       </body>
     </html>
   );
